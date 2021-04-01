3004960af760414f1f16acd1198c1854be62cc1b
ohnhingi1 <long1122334400@gmail.com>
2021-04-01 11:53:06 +0700

    update

---
+++ module-2/nhap/src/nhap.java
@@ -0,0 +1,14 @@
+public class nhap{
+    public static void main(String[] args) {
+
+        m(a);
+        System.out.println(a);
+
+
+
+    }
+    static void m(int x){
+        x=101;
+
+    }
+}
